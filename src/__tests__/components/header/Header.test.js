ede725b69224046c0a72e20dd6b6943359261dd9
Joffrey Trebot <trebotjoffrey@gmail.com>
2022-04-26 17:20:16 +0200

    feat(header): implement pic of me

--- src/__tests__/components/header/Header.test.js
+++ src/__tests__/components/header/Header.test.js
@@ -2,6 +2,7 @@ import Header from '../../../components/header/Header';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import CTA from '../../../components/header/CTA';
 
 configure({ adapter: new Adapter() });
 
@@ -13,23 +14,35 @@ describe('Header', () => {
     wrapper = shallow(<Header />);
   });
   test('render container with header informations', () => {
-    expect(wrapper.find('div').hasClass('container header__container')).toBe(true);
-    expect(wrapper.find('div').children().length).toEqual(3);
-    expect(wrapper.find('div').childAt(0).type()).toEqual('h5');
-    expect(wrapper.find('div').childAt(1).type()).toEqual('h1');
-    expect(wrapper.find('div').childAt(2).type()).toEqual('h5');
+    expect(wrapper.find('#header').hasClass('container header__container')).toBe(true);
+    expect(wrapper.find('#header').children().length).toEqual(5);
+    expect(wrapper.find('#header').childAt(0).type()).toEqual('h5');
+    expect(wrapper.find('#header').childAt(1).type()).toEqual('h1');
+    expect(wrapper.find('#header').childAt(2).type()).toEqual('h5');
   });
 
   test('render text in first h5 tag', () => {
-    expect(wrapper.find('div').childAt(0).text()).toBe("Hello I'm");
+    expect(wrapper.find('#header').childAt(0).text()).toBe("Hello I'm");
   });
 
   test('render name in h1 tag', () => {
-    expect(wrapper.find('div').childAt(1).text()).toBe("Joffrey Trébot");
+    expect(wrapper.find('#header').childAt(1).text()).toBe("Joffrey Trébot");
   });
 
   test('render job name in last h5 tag', () => {
-    expect(wrapper.find('div').childAt(2).text()).toBe("Software Engineer");
-    expect(wrapper.find('div').childAt(2).hasClass('text-light')).toBe(true);
+    expect(wrapper.find('#header').childAt(2).text()).toBe("Software Engineer");
+    expect(wrapper.find('#header').childAt(2).hasClass('text-light')).toBe(true);
+  });
+
+  test('render CTA for download CV', () => {
+    expect(wrapper.contains(<CTA />)).toBe(true);
+  });
+
+  test('render a pic of me', () => {
+    expect(wrapper.find('#picOfMe').hasClass('me')).toBe(true);
+    expect(wrapper.find('#picOfMe').children().length).toEqual(1);
+    expect(wrapper.find('#picOfMe').childAt(0).type()).toEqual('img');
+    expect(wrapper.find('#picOfMe').childAt(0).props().src).toEqual('me.png');
+    expect(wrapper.find('#picOfMe').childAt(0).props().alt).toEqual('me');
   });
 });
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,23 @@
 
 import React, { Component } from 'react'
+import CTA from './CTA'
 import './header.css'
+import ME from '../../assets/me.png'
 
 export class Header extends Component {
   static propTypes = {}
 
   render() {
     return (
-      <div className='container header__container'>
+      <div id="header" className='container header__container'>
         <h5>Hello I'm</h5>
         <h1>Joffrey Trébot</h1>
         <h5 className='text-light'>Software Engineer</h5>   
+        <CTA />
+
+        <div id="picOfMe" className='me'>
+          <img src={ME} alt='me' />
+        </div>
       </div>
     )
   }
